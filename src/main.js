7868a807b2d47d60502b7b6c4a69c259846bd53d
张恒 <syl1998@hotmail.com>
2019-11-26 15:39:20 +0800

    修正例子中的bug

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui';
 import Echarts from 'echarts'
 // 当切仅当开发模式时，引入css，不然webpack还是会将element的字体和css打包进来，比较致命
 // 当然这里也要判断一下是否开启了cdn，如果没有开启cdn却也忽略了css的引入，那就更致命了
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
     if (!require('../config/index').build.cdn) {
         require('element-ui/lib/theme-chalk/index.css')
     }
+}else{
+    require('element-ui/lib/theme-chalk/index.css')
 }
 
 Vue.use(ElementUI);
