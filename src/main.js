d45332badac40011e0c432496d47ecd3e60e1802
umbrella22 <1035765115@qq.com>
2020-12-11 16:28:27 +0800

    更新依赖，准备尝试迁移到vue3

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
-import Echarts from 'echarts'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI);
@@ -10,7 +9,6 @@ Vue.use(ElementUI);
 
 
 
-Vue.prototype.$echarts = Echarts
 
 
 
