58e8f586ca542a7c2c110ed799509c2fc8f280e4
umbrella22 <1035765115@qq.com>
2021-06-25 11:55:53 +0800

    更新依赖，试更新到"webpack-dev-server": "^4.0.0-beta.3"

--- packageTools/dev-running.js
+++ packageTools/dev-running.js
@@ -16,10 +16,10 @@ const viteConifg = require("./vite.config")
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 const DevServerConfig = {
-    clientLogLevel: 'warning',
+    // clientLogLevel: 'warning',
     // 当使用history出现404时则自动调回index页
     historyApiFallback: true,
-    contentBase: path.join(__dirname, "../dist"),
+    // contentBase: path.join(__dirname, "../dist"),
     // 热加载模式
     hot: true,
     // 启用gzip
@@ -28,18 +28,18 @@ const DevServerConfig = {
     host: HOST || config.dev.host,
     // 设置是否自动打开浏览器
     open: config.dev.autoOpenBrowser,
-    // 当编译器出现错误时，在全屏覆盖显示错误位置
-    overlay: config.dev.errorOverlay
-        ? { warnings: false, errors: true }
-        : false,
+    // // 当编译器出现错误时，在全屏覆盖显示错误位置
+    // overlay: config.dev.errorOverlay
+    //     ? { warnings: false, errors: true }
+    //     : false,
     // 从config文件中读取端口代理设置
     proxy: config.dev.proxyTable,
-    // 启用简洁报错
-    quiet: true,
-    // 启用监听文件变化
-    watchOptions: {
-        poll: config.dev.poll,
-    }
+    // // 启用简洁报错
+    // quiet: true,
+    // // 启用监听文件变化
+    // watchOptions: {
+    //     poll: config.dev.poll,
+    // }
 }
 const viteDevConfig = {
     server: {
@@ -68,15 +68,12 @@ function statr() {
                 server.listen(port)
             } else {
                 webpackConfig.plugins.push(new FriendlyErrorsPlugin({
-                    compilationSuccessInfo: {
-                        messages: [`您的项目已成功启动`],
-                        notes: [`本机访问请在vscode的终端中按住左ctrl键点击: http://127.0.0.1:${port} \n `, `局域网访问地址: http://${utils.getNetworkIp()}:${port}`],
-                    },
+                    compilationSuccessInfo: {},
                     onErrors: config.dev.notifyOnErrors
                         ? utils.createNotifierCallback()
                         : undefined
                 }))
-                WebpackDevServer.addDevServerEntrypoints(webpackConfig, DevServerConfig);
+                // WebpackDevServer.addDevServerEntrypoints(webpackConfig, DevServerConfig);
                 const compiler = webpack(webpackConfig)
                 const server = new WebpackDevServer(compiler, DevServerConfig)
                 server.listen(port)
